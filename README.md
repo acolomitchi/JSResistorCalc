10abed219756449195b5381475f48d3d31de95c6
acolomitchi <acolomitchi@gmail.com>
2016-07-21 12:46:36 +1000

    Release - perhaps v1.0? - the functionality seems to be round enough to my liking.

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Calculator for a combination of resistors having a value as close as possible to a specified one.
 
 Uses:
-- JQuery for the UI controller
 - JavaScript workers to perform the computations on separated threads
+- JQuery and JQUeryUI for the UI controller
 - Heavy use of functional programming, JavaScript style.
 
 An online accessible version is already available at:
@@ -12,15 +12,29 @@ http://caffeineowl.com/electronics/calcs/rescomb/
 See [LICENCE.html](the license - CC-BY-*NC*).
 
 ## User guide
-Enter the resistor value you need in Ohms (sorry, no unit parsing/converter now), pick your maximum number of resistors in the combination, choose the resistor series from which the script will choose the resistors in the combination, hit the "Compute" button.
-
+Enter the resistor value you need in Ohms (sorry, no unit parsing/converter now), 
+pick the maximum number of resistors in the combination,
+  choose the resistor values available to make combination 
+  (you can even pick custom subsets of E12 or E24),
+  adjust the combination type (parallel only, series only or both), 
+  hit the "Compute" button.
+  
 *Notes*:
-* Every additional resistor in the combination will improve
-  the closeness of the result by two orders of magnitude, but it will
-  take longer to scan the possible combination. The computation time
-  is why "E12" series allows you to ask for combinations of max 4 resistors 
-  (0.0001%=1e-6 relative precision), but the "E24" will allow you to
-  go only with max 3 (roughly 0.0005%=5e-6 relative precision)</p>
-* Mind you, the closeness of the combination value to your required one
+<ol>
+  <li> Every additional resistor in the combination will improve
+  the quality of the resulted values by roughly two orders of magnitude. 
+  However asking for combinations made of a higher number of resistors
+  will cause longer searches for combinations (a supra-exponential grow).
+  The calculator will restrict the maximum number of resistors in combination
+  based on the number of the available/input resistor values.<ul>
+    <li>For input sets up to 24 values, you can request combinations up to 6 resistors.</li>
+  <li>For input sets up to 36 values, the maximum number of resistor in the combination is 5</li>
+  <li>For up to 72 input values (full E12 included), combinations up to 4 resistors may be requested</li>
+  <li>For more than 72 values, only combinations of 3 resistors will be considered</li>
+  </ul>
+  <li>
+  
+<li> Mind you, the closeness of the combination value to your required one
   doesn't guarantee that you'll actually obtain that value using
-  resistors with a tolerance of &plusmn;10% (E12) or &plusmn;5% (E24).
+  resistors with a tolerance of &plusmn;10% (E12) or &plusmn;5% (E24).</li>
+</ol>
---
+++ ctrl.js
@@ -0,0 +1,311 @@
+  function Ctrl() {
+    this.rcWorker=null;
+    this.valSet=$rc.E12Series; // the chosen value set
+
+    var owner=this; // to have it as a variable in closures
+    
+    this.updateData=function(data, done) {
+      var numCombs=$("#numCombs");
+      numCombs.html("# within "+Math.round(this.tolerance*1000)/10+"% of target: "+
+        (null!=data ? data.combs : 0)
+      );
+      if(done) {
+        $("#cancelSupport").empty();
+        $("#cancelSupport").html("Done!");
+      }
+      var resTBody=$("#resTable tbody");
+      resTBody.empty();
+      var ownerCtrl=this;
+      if(null!==data && "undefined"!=typeof(data.vals) && $rc.util.isArray(data.vals)) {
+        data.vals.forEach(
+          function(e) {
+            var minTxt=
+              e.min+"<br>("+Math.round((e.min-ownerCtrl.target)/ownerCtrl.target*1000000)/10000+"%)";
+            ;
+            var maxTxt=
+              e.max+"<br>("+Math.round((e.max-ownerCtrl.target)/ownerCtrl.target*1000000)/10000+"%)";
+            ;
+            var valTxt=(new $rc.Val(e.v)).toString()+"<br>("+Math.round(e.v*1000)/1000+")";
+            var eps=(e.v-ownerCtrl.target)/ownerCtrl.target;
+            eps=Math.round(eps*1000000)/10000;
+            resTBody.append(
+              "<tr><td>"+e.c+"</td>"+
+              "<td>"+valTxt+"</td>"+
+              "<td>"+eps+"</td>"+
+              "<td>"+minTxt+"</td>"+
+              "<td>"+maxTxt+"</td>"+
+              "</tr>"
+            );
+          }
+        );
+      }
+    }
+    function showCustomSubsetDialog(subset, doneCallback) {
+      var d=$("#custom-choice-dialog");
+      var nRows=subset.nRows;
+      var nCols=subset.v.length/nRows;
+      function valueIx(rIx, cIx) {
+        return cIx*nRows+rIx;
+      };
+      function valueId(rIx, cIx) {
+        return "cvId"+valueIx(rIx,cIx);
+      };
+      var str="<table style='border:none;width:100%'><tr>";
+      for(var cIx=0;cIx<nCols;cIx++) {
+        var has=false;
+        for(var rIx=0; rIx<nRows && !has; rIx++) {
+          var valObj=subset.v[valueIx(rIx, cIx)];
+          has=valObj.s;
+        }
+        str+=
+            "<td align='right'>&darr;&nbsp;<input type='checkbox' value='"+cIx
+          + "' id='reset"+cIx+"'"
+        ;
+        if(has) {
+          str+="checked='checked'";
+        }
+        str+="/></td>";
+      }
+      str+="</tr>";
+      for(var rIx=0; rIx<nRows;rIx++) {
+        str+="<tr>";
+        for(var cIx=0; cIx<nCols;cIx++) {
+          var vIx=valueIx(rIx,cIx);
+          var vId=valueId(rIx, cIx);
+          var valObj=subset.v[vIx];
+          str+="<td align='right'><label for='"+vId+"'>"+valObj.v.toString()+"</label>";
+          str+="<input type='checkbox' id='"+vId+"' value='"+vIx+"'";
+          if(valObj.s) {
+            str+=" checked='checked'";
+          }
+          str+="/></td>";
+        }
+        str+="</tr>";
+      }
+      str+="</table>";
+      d.html(str);
+      for(var cIx=0;cIx<nCols;cIx++) {
+        d.find("#reset"+cIx).change(function(e) {
+          var enAll=$(this).is(':checked');
+          var cIx=$(this).val();
+          for(var i=0;i<nRows;i++) {
+            d.find("#"+valueId(i,cIx)).attr("checked",enAll);
+            // Darn, changing it programatically doesn't trigger an event. 
+            // Well, adjust the corresponding value manually
+            var vIx=valueIx(i,cIx);
+            subset.v[vIx].s=enAll;
+          }
+        });
+        for(var rIx=0;rIx<nRows;rIx++) {
+          var chBox=d.find("#"+valueId(rIx,cIx));
+          chBox.change(function(e) {
+            var sel=$(this).is(":checked");
+            var vIx=$(this).val()-0; // make it number
+            subset.v[vIx].s=sel;
+          });
+          // chBox.checkboxradio();
+        }
+      }
+      d.dialog(
+        {
+          resizable: false,
+          height: "auto",
+          width: 460,
+          maxHeight : 520,
+          modal: true,
+          autoOpen: false,
+          buttons: {
+            "OK": function() {
+              $( this ).dialog( "close" );
+              $("custom-choice-dialog").empty();
+              var valSet=[];
+              subset.v.forEach(valObj=>{
+                if(valObj.s) {
+                  valSet.push(valObj.v.getValue());
+                }
+              });
+              doneCallback(valSet);
+            },
+            Cancel: function() {
+              $( this ).dialog( "close" );
+              $("custom-choice-dialog").empty();
+            }
+          }
+        }
+      );
+      d.dialog("open");
+    }
+    this.showE12subset=function() {
+      var resSet=[];
+      var saved=window.sessionStorage['E12_subset'];
+      saved=
+        (typeof(saved)==="undefined" || null===saved) 
+        ? [] 
+        : saved.split(',')
+      ;
+      $rc.E12Series.forEach(function(e) {
+        var ix=saved.binarySearch(e);
+        resSet.push({v:new $rc.Val(e),s:(ix>=0)});
+      });
+      var subset={nRows:12,v:resSet};
+      var ctrlOwner=this;
+      showCustomSubsetDialog(subset, function(valSet) {
+        ctrlOwner.valSet=valSet;
+        ctrlOwner.adjustMaxNum();
+        window.sessionStorage['E12_subset']=valSet;
+      });
+      
+
+    }
+    
+    this.showE24subset=function() {
+      var resSet=[];
+      var saved=window.sessionStorage['E24_subset'];
+      saved=
+          (typeof(saved)==="undefined" || null===saved) 
+        ? [] 
+        : saved.split(',')
+      ;
+      $rc.E24Series.forEach(function(e) {
+        var ix=saved.binarySearch(e);
+        resSet.push({v:new $rc.Val(e),s:(ix>=0)});
+      });
+      var subset={nRows:24,v:resSet};
+      var ctrlOwner=this;
+      showCustomSubsetDialog(subset, function(valSet) {
+        ctrlOwner.valSet=valSet;
+        ctrlOwner.adjustMaxNum();
+        window.sessionStorage['E24_subset']=valSet;
+      });
+    }
+
+    this.compute=function() {
+      if(null!=this.rcWorker) {
+        this.rcWorker.terminate();
+        this.rcWorker=null;
+      }
+      this.updateData(null, false);
+      var target=$("#resistorValue").val();
+      if(false==$rc.util.isNum(target)) {
+        alert("Expecting a number as the 'Desired value'");
+        return;
+      }
+      this.target=target-0;
+      this.rcWorker=new Worker("worker.js");
+      var ownerCtrl=this;
+      this.rcWorker.onmessage=function(e) {
+        if(typeof(e.data)!="undefined") {
+          var type=e.data.type;
+          if("progress"==type || "done"==type) {
+            ownerCtrl.updateData(e.data, "done"==type);
+          }
+          if("done"==type) {
+            this.rcWorker.terminate();
+            this.rcWorker=null;
+          }
+        }
+      }
+      this.rcWorker.onerror=function(e) {
+        // TODO handle it better
+        alert(e.message);
+      }
+      var series=$("#rSeries").val();
+      this.tolerance=(series.startsWith("E24") ? 0.05 : 0.1);
+      series=this.valSet;
+      var combType=$('#combType').val();
+      var owner=this;
+      $("#cancelSupport").html("<button id='cancelBtn'>Cancel</button>");
+      $("#cancelSupport").find("#cancelBtn").click(
+        function(e) {
+          if(null!=owner.rcWorker) {
+            owner.rcWorker.terminate();
+            owner.rcWorker=null;
+            setTimeout(
+              function() {
+                $("#cancelSupport").html("Interrupted!");
+              },850
+            );
+          }
+        }
+      );
+      this.rcWorker.postMessage({
+        cmd:"start",
+        numRes:$("#numComponents").val(),
+        target: this.target,
+        tolerance:this.tolerance,
+        srcVals:series,
+        combType:combType
+      });
+    }
+    
+    this.adjustMaxNum=function() {
+      if(typeof(this.valSet)!="undefined") {
+        var maxNum=6;
+        if(this.valSet.length<=24) {
+          maxNum=6;
+        }
+        else if(this.valSet.length<=36) {
+          maxNum=5;
+        }
+        else if(this.valSet.length<=72) {
+          maxNum=4;
+        }
+        else {
+          maxNum=3;
+        }
+        var numSelected=$("#numComponents").find(":selected").val();
+        if(numSelected>maxNum) {
+          numSelected=maxNum;
+        }
+        for(i=1; i<=maxNum; i++) {
+          var opt=$("#numComponents").find("[value="+i+"]");
+          opt.prop("disabled",false);
+          if(i==numSelected) {
+            opt.prop("selected", true);
+          }
+        }
+        for(i=maxNum+1; i<=6; i++) {
+          $("#numComponents").find("[value="+i+"]").prop("disabled",true); 
+        }
+      }
+    }
+    
+    $("#resistorValue").on("input",
+      function(e) {
+        var valStr=$("#resistorValue").val();
+        var validVal=$rc.util.isNum(valStr) && (valStr-0)>0;
+        $("#computeButton").prop('disabled', !validVal);
+      }
+    );
+    var owner=this;
+    $("#rSeries").find("option").on("click",
+      function(e) {
+        var v=$("#rSeries").val();
+        if("E24"==v) {
+          owner.valSet=$rc.E24Series;
+          owner.adjustMaxNum();
+        }
+        else if("E12"==v) {
+          owner.valSet=$rc.E12Series;
+          owner.adjustMaxNum();
+        }
+        else if("E12_subset"==v) {
+          owner.showE12subset();
+        }
+        else if("E24_subset"==v) {
+          owner.showE24subset();
+        }
+      }
+    );
+    $("#computeButton").prop('disabled', true);
+    $("#resistorValue").val("");
+    $("#numComponents").find("[value=3]").prop("selected", true);
+    $("#rSeries").find("[value=E12]").prop("selected", true);
+    this.adjustMaxNum();
+    var owner=this;
+    $("#computeButton").click(
+      function(){
+        owner.compute();
+      }
+    );
+  }
